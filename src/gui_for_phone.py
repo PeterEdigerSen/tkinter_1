c30ffeb7c00c274d575c4154f001f51ef364d453
peterediger <peter.ediger@delta-v.us>
2021-11-25 22:31:03 +0100

    Blinking digits, Quit button

--- src/gui_for_phone.py
+++ src/gui_for_phone.py
@@ -1,14 +1,29 @@
-import tkinter as tk
-import os
 import functools
+import tkinter as tk
 
 
-def add_digit(msg_par, event=None):
-    msg.configure(text=(msg.cget('text') + str(msg_par)))
+class Key(tk.Button):
+    def white(self):
+        self.configure(fg='white')
+
+    def red(self):
+        self.configure(fg='red')
+
+
+def add_digit(digit, event=None):
+    msg.configure(text=(msg.cget('text') + str(digit)))
+    current_key = keys[digit]
+    for i in range(0, 1000, 100):
+        current_key.after(i, current_key.white)
+        current_key.after(i + 50, current_key.red)
 
 
 def remove_digit(event=None):
     msg.configure(text=msg.cget('text')[:-1])
+
+
+def destroy_master(event=None):
+    master_widget.destroy()
 
 
 master_widget = tk.Tk()
@@ -23,49 +38,38 @@ def grid_place(digit):
         return (digit - 1) // 3, (digit - 1) % 3, 1
 
 
-for i in range(1, 10):
+keys = []
+for i in range(0, 10):
     r, c, cs = grid_place(i)
-    tk.Button(frame,
-              text=i,
-              command=functools.partial(add_digit, i),
-              fg="red",
-              bg='grey',
-              width=5,
-              height=3,
-              relief=tk.GROOVE). \
-        grid(row=r,
-             column=c,
-             columnspan=cs,
-             sticky=tk.E + tk.W,
-             padx=5,
-             pady=5)
+    keys.append(
+        Key(frame,
+            text=i,
+            command=functools.partial(add_digit, i),
+            fg="red",
+            bg='grey',
+            width=5,
+            height=3,
+            relief=tk.GROOVE))
+    keys[i].grid(row=r,
+                 column=c,
+                 columnspan=cs,
+                 sticky=tk.E + tk.W,
+                 padx=5,
+                 pady=5)
     master_widget.bind(str(i), functools.partial(add_digit, i))
 
 tk.Button(frame,
-          text=0,
-          command=functools.partial(add_digit, 0),
+          text='DEL',
+          command=remove_digit,
           fg="red",
           width=5,
           height=3). \
     grid(row=3,
-         column=0,
+         column=2,
          columnspan=2,
          sticky=tk.E + tk.W,
          padx=5,
          pady=5)
-
-b = tk.Button(frame,
-              text='DEL',
-              command=remove_digit,
-              fg="red",
-              width=5,
-              height=3)
-b.grid(row=3,
-       column=2,
-       columnspan=2,
-       sticky=tk.E + tk.W,
-       padx=5,
-       pady=5)
 master_widget.bind('d', remove_digit)
 
 msg = tk.Label(frame,
@@ -78,5 +82,19 @@ msg.grid(row=4,
          sticky=tk.E + tk.W,
          padx=5,
          pady=5)
+
+tk.Button(frame,
+          text='QUIT',
+          command=destroy_master,
+          fg="red",
+          width=5,
+          height=3). \
+    grid(row=5,
+         column=0,
+         columnspan=3,
+         sticky=tk.E + tk.W,
+         padx=5,
+         pady=5)
+master_widget.bind('q', destroy_master)
 
 master_widget.mainloop()
