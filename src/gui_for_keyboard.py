eb1f01fdc3d08c63bb8be313d8e077d94ac37c88
peterediger <peter.ediger@delta-v.us>
2021-12-02 08:37:19 +0100

    Images for Button/Label

--- src/gui_for_keyboard.py
+++ src/gui_for_keyboard.py
@@ -1,5 +1,5 @@
 import functools
-from tkinter import Button, Tk  # Python 3
+from tkinter import Button, Tk, PhotoImage, LEFT, RIGHT, TOP, CENTER, BOTTOM  # Python 3
 
 
 def func(param, event=None):
@@ -15,11 +15,20 @@ root2 = Tk(className='Oho 2')
 root1.geometry('200x200+500+500')
 root2.geometry('200x200+800+500')
 
-Button(root1, text="Hello 1", command=functools.partial(func, '1')).pack()
-Button(root2, text="Hello 2", command=functools.partial(func, '2')).pack()
+rose1 = PhotoImage(file='../files/rose_1.png')
+rose2 = PhotoImage(file='../files/rose_2.png')
+
+Button(root1, image=rose1, text='Label 2', compound=CENTER,
+       width=50, height=50,
+       command=functools.partial(func, '1')). \
+    pack(side=LEFT)
+Button(root1, image=rose2, text='Label 1', compound=LEFT,
+       width=50, height=50,
+       command=functools.partial(func, '2')). \
+    pack(side=RIGHT)
 
 root1.bind('a', functools.partial(func, '1'))
-root2.bind('b', functools.partial(func, '2'))
+root1.bind('b', functools.partial(func, '2'))
 root2.bind('c', functools.partial(func, '3'))
 
 root1.mainloop()
--- src/gui_for_size.py
+++ src/gui_for_size.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title('RooT')
 pixelVirtual = tk.PhotoImage(width=1, height=1)
+rose = tk.PhotoImage(file='../files/download.png')
 fr = tk.Frame(root)
 fr.pack()
-butt = tk.Label(fr, image=pixelVirtual, bg='blue', width='5i',height='5i')
+butt = tk.Label(fr, image=rose)
 butt.pack()
 
 root.mainloop()
