dcf180ae1fdfcf0fe0396bce0087e0cf2986974b
peterediger <peter.ediger@delta-v.us>
2021-12-02 09:01:32 +0100

    Images for Button/Label (version2)

--- src/gui_icon.py
+++ src/gui_icon.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 
 root = tk.Tk()
-img = tk.PhotoImage(file="Documents-icon.png")
+img = tk.PhotoImage(file="../files/rose_1.png")
 ## Labels and pack
 
 ## create window
 root.geometry("250x400")
-root.iconbitmap(bitmap=img)
+root.iconbitmap("../files/download.png")
 root.title("App")
 root.resizable(0, 0)
 root.config(bg="blue")
+root.mainloop()
